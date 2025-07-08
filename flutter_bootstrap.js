9bb556eb7a4ae593085978c1d2a865a39500c3be
PrinceCraox <aryansm832@gmail.com>
2025-07-08 22:55:33 +0530

    paypel integreted

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2931724349"
+    serviceWorkerVersion: "1732023946"
   }
 });
